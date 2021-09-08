74369dcc6f632ec60d461248bd8753d60b31cfe3
tragiclov <frijacblu@yahoo.com>
2021-09-07 22:21:08 -0700

    step 1 counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -47,12 +47,13 @@ STEP 6:
 */
 
 import React from 'react'; /* STEP 0 */
-
+  
 export default function Counter() {
   /* STEP 1 */
-
+  const [count, setCount] = useState(0);
   const increment = () => {
     /* STEP 4 */
+    setCount(count + 1)
   };
   const decrement = () => {
     /* STEP 5 */
